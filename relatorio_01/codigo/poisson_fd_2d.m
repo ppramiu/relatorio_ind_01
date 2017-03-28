98710451a1041ff4753e692d1aa0d5c47e0d7c2e
ppramiu <ppramiu@gmail.com>
2017-03-27 23:50:23 -0300

    novo octave

--- relatorio_01/codigo/poisson_fd_2d.m
+++ relatorio_01/codigo/poisson_fd_2d.m
@@ -1,25 +1,40 @@
 % Poisson equation -\nabla² u = f with dirichlet conditions u = 0
-mx = 9;
-my = 9;
+clear all;
+mx = 4;
+my = 3;
 hx = 1/(mx-1);
 hy = 1/(my-1);
+
+x_points = linspace(0,1,(mx));
+y_points = linspace(0,1,(my));
+
+for i=1:mx
+  for j=1:my
+    u(i,j) =  (x_points(i)^2-x_points(i)^4)*(y_points(j)^4-y_points(j)^2);
+  end
+end
+
+u=reshape(u,1,[])
+
 A = zeros(mx*my);
-u = 
 for i = 1:mx*my 
-  A(i,i) = 2*(hx/hy + hy/hx);
+  A(i,i) = 2*(1/hy^2 + 1/hx^2);
 end
 
 for i=1:my-1
   for j=1:mx
-    A(i+(j-1)*my,i+(j-1)*my+1) = -hy/hx;
-    A(i+(j-1)*my+1,i+(j-1)*my) = -hy/hx;
+    A(i+(j-1)*my,i+(j-1)*my+1) = -1/hx^2;
+    A(i+(j-1)*my+1,i+(j-1)*my) = -1/hx^2;
   end
 end
 for i = 1:my
   for j = 1:mx-1
-    A(i+(j-1)*my,i+j*my) = -hx/hy;
-    A(i+j*my,i+(j-1)*my) = -hx/hy;
+    A(i+(j-1)*my,i+j*my) = -1/hy^2;
+    A(i+j*my,i+(j-1)*my) = -1/hy^2;
   end
 end
+
+b = A*u';
+A\b
 
 spy(A)
